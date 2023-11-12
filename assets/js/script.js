b83070e0391268b867a58e1f854729fb2716abc2
Ewelina Sobora <e@e-dot.uk>
2023-11-12 20:11:46 +0000

    feat: init planner table

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  const workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+  const calendar = $("#calendar-container");
+  const currentDay = $("#currentDay");
+  let today = dayjs();
+  currentDay.text(today.format("dddd, D MMMM YYYY"));
+
+  // function that creates the calendar
+  function createCalendar() {
+    var table = $('<table>').addClass('table').appendTo(calendar);
+    // rows for each work hour
+    workHours.forEach(hour => {
+      var row = $('<tr>').appendTo(table);
+      // cells for Time, Event, and Save
+      $('<td>').text(hour).appendTo(row);
+      $('<td>').append($('<textarea>').addClass('description')).appendTo(row);
+      $('<td>').append($('<button>').addClass('saveBtn').text('Save')).appendTo(row);
+    });
+  }
+
+  createCalendar();
+});
