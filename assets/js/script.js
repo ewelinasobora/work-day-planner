ee792f9862aedd7b268bd25fc27d0bf120cd83c0
Ewelina Sobora <e@e-dot.uk>
2023-11-13 20:03:19 +0000

    feat: each timeblock based on past, present, and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,32 @@
 $(document).ready(function () {
-  const workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+  const workHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM"];
   const calendar = $("#calendar-container");
   const currentDay = $("#currentDay");
   let today = dayjs();
   currentDay.text(today.format("dddd, D MMMM YYYY"));
-
+  let currentHour = today.format("h A");
   // function that creates the calendar
   function createCalendar() {
-    var table = $('<table>').addClass('table-primary table-responsive').appendTo(calendar);
+
+
+    let table = $('<table>').appendTo(calendar);
     // rows for each work hour
     workHours.forEach(hour => {
-      var row = $('<tr>').appendTo(table);
+      let hour24 = Number(moment(hour, ["h A"]).format("H"));
+      let currentHour24 = Number(moment(currentHour, ["h A"]).format("H"));
+      let row = $('<tr>').appendTo(table);
       // cells for Time, Event, and Save
-      $('<td>').text(hour).appendTo(row);
-      $('<td>').append($('<textarea>').addClass('description')).appendTo(row);
-      $('<td>').append($('<button>').addClass('saveBtn').text('Save')).appendTo(row);
+      let timeSlot = $('<td>').addClass("time-block hour").text(hour).appendTo(row);
+      let description = $('<td>').addClass("row description").append($('<textarea>')).appendTo(row);
+      let saveButton = $('<td>').addClass("saveBtn").appendTo(row).append($('<button>').addClass("btn").text("Save"));
+
+      if (currentHour24 === hour24) {
+        description.addClass("present");
+      } else if (currentHour24 > hour24) {
+        description.addClass("past");
+      } else if (currentHour24 < hour24) {
+        description.addClass("future");
+      }
     });
   }
 
